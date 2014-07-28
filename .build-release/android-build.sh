746088f57cbb8459c66a09348e7f03dd2b816541
b3nroth <b3nroth@gmail.com>
2014-07-28 16:16:29 -0400

    remove extraneous exports from build.sh script
    several exports were not being used by script. Also added comments to remind user to set local
    SDK and NDK paths.

--- .build-release/android-build.sh
+++ .build-release/android-build.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Symbols expected by script
-#
-# export ANDROID_ADT=/opt/adt-bundle-linux-x86_64
-# export ANDROID_ANT=$ANDROID_ADT/eclipse/plugins/org.apache.ant_1.8.3.v20120321-1730/bin/ant
-# export ANDROID_SDK=$ANDROID_ADT/sdk
-# export ANDROID_NDK=/opt/android-ndk-r8e
-# export JAVA_JDK=/opt/jdk1.6.0_45/bin
-#
-# export PATH=$JAVA_JDK:$ANDROID_NDK:$ANDROID_ADT:$ANDROID_ANT:$ANDROID_SDK/tools:$PATH
-# export SRC_ROOT=$WORKSPACE
-# export AUTOMATED_BUILD=1
-# export JNI_ROOT=$WORKSPACE/scandroid/support
-#
-# export GRADLE_USER_HOME="$WORKSPACE/scandroid"
-#
 
 echo "sdk.dir=$ANDROID_SDK" > local.properties
 echo "ndk.dir=$ANDROID_NDK" >> local.properties
